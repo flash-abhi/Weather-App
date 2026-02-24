414c2527cd13b82330ded880570d6c474f52fb15
Abhishek <abhishekchauhan9119001283@gmail.com>
2026-02-24 14:28:57 +0530

    Created the GetCurrentUser controller and setted the route for it

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/auth.controller.js";
+import { getCurrentUser, loginUser, registerUser } from "../controllers/auth.controller.js";
 
 const Authrouter = express.Router();
 
 Authrouter.post("/register", registerUser);
 Authrouter.post("/login", loginUser);
-
+Authrouter.get("/me",getCurrentUser);
 export default Authrouter; 
