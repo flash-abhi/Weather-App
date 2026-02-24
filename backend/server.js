1e26f6672d713eb3e0ea325f9b9aba3911334aa5
Abhishek <abhishekchauhan9119001283@gmail.com>
2026-02-24 11:54:28 +0530

    in Backend i have injected the cookies while login in and removing while logout

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,18 @@ import authRoutes from "./routes/auth.route.js";
 import cityRoutes from "./routes/city.route.js";
 import aiRoutes from "./routes/ai.route.js";
 import { connectDb } from "./utils/db.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 const Port = process.env.PORT || 3000;
-app.use(cors()); 
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true, 
+})); 
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/cities", cityRoutes);
